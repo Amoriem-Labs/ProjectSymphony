0a2db2dc43902648efd2e75a9917f4ed7a2284d6
Daniellecampisi <117215714+Daniellecampisi@users.noreply.github.com>
2025-03-28 13:01:30 -0400

    Week1 Day2 Edits

--- Assets/VN/Scripts/Week1/W1D2.cs
+++ Assets/VN/Scripts/Week1/W1D2.cs
@@ -26,162 +26,164 @@ public class W1D2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (dialogueManager.isW1D2 && !isStartedD2)
+        if (dialogueManager.isW1D2)
         {
-            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.0" }));
-            isStartedD2 = true;
-            startTESTS = true;
-        }
-
-        if (startTESTS)
-        {
-
-
-
-            if (!string.IsNullOrEmpty(dialogueManager.selectedOption) && DP1_1)
+            if (dialogueManager.isW1D2 && !isStartedD2)
             {
-                string currselectedOption = dialogueManager.selectedOption;
-
-                // restart manager's selected option
-                DP1_1 = false;
-                dialogueManager.selectedOption = "";
-
-                if (currselectedOption == "Call out to them.")
-                {
-                    StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.01A", "W1D2.2" }));
-                    DP1_2 = true;
-
-                }
-                else if (currselectedOption == "Sneak up on them.")
-                {
-                    DP1_2 = true;
-                    StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.01B", "W1D2.2" }));
-                }
-
-
+                StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.0" }));
+                isStartedD2 = true;
+                startTESTS = true;
             }
 
-            if (!string.IsNullOrEmpty(dialogueManager.selectedOption) && DP1_2)
+            if (startTESTS)
             {
-                string currselectedOption = dialogueManager.selectedOption;
-
-                // restart manager's selected option
-                dialogueManager.selectedOption = "";
-
-                if (currselectedOption == "Maybe I can see where people rehearse?")
-                {
-                    if (classroom && library && park)
-                    {
-                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02A" }));
-                        DP1_3 = true;
-                        DP1_2 = false;
-
-                    }
-                    else if (reherse)
-                    {
-                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.2E" }));
-                    }
-                    else
-                    {
-                        reherse = true;
-                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02A", "W1D2.2F" }));
-                    }
-
-                }
-                else if (currselectedOption == "The park!")
-                {
-                    if (classroom && library && reherse)
-                    {
-                        DP1_3 = true;
-                        DP1_2 = false;
-                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02B" }));
-                    }
-                    else if (park)
-                    {
-                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.2E" }));
-                    }
-                    else
-                    {
-                        park = true;
-                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02B", "W1D2.2F" }));
-                    }
-                }
-
-                else if (currselectedOption == "I think seeing a classroom would be nice before I'm forced in there against my will.")
-                {
-                    if (library && park && reherse)
-                    {
-                        DP1_3 = true;
-                        DP1_2 = false;
-                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02C" }));
-                    }
-                    else if (classroom)
-                    {
-                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.2E" }));
-                    }
-                    else
-                    {
-                        classroom = true;
-                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02C", "W1D2.2F" }));
-                    }
-                }
-                else if (currselectedOption == "Could we go to the library?")
-                {
-                    if (classroom && park && reherse)
-                    {
-                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02D" }));
-                        DP1_3 = true;
-                        DP1_2 = false;
-                    }
-                    else if (library)
-                    {
-                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.2E" }));
-                    }
-                    else
-                    {
-                        library = true;
-                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02D", "W1D2.2F" }));
-                    }
-                }
 
 
-                if (classroom && library && park && reherse)
-                {
-                    DP1_3 = true;
-                }
 
-
-            }
-
-            if (DP1_3)
-            {
-                if(dialogueManager.activeDialogue == false)
+                if (!string.IsNullOrEmpty(dialogueManager.selectedOption) && DP1_1)
                 {
                     string currselectedOption = dialogueManager.selectedOption;
 
                     // restart manager's selected option
-                    DP1_3 = false;
+                    DP1_1 = false;
                     dialogueManager.selectedOption = "";
 
-                    StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.03" }));
-                    dialogueManager.isW1D2 = false;
-                    startTESTS = false;
-                    dialogueManager.isW1D3 = true;
-                     END = true;
+                    if (currselectedOption == "Call out to them.")
+                    {
+                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.01A", "W1D2.2" }));
+                        DP1_2 = true;
+
+                    }
+                    else if (currselectedOption == "Sneak up on them.")
+                    {
+                        DP1_2 = true;
+                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.01B", "W1D2.2" }));
+                    }
+
 
                 }
 
+                if (!string.IsNullOrEmpty(dialogueManager.selectedOption) && DP1_2)
+                {
+                    string currselectedOption = dialogueManager.selectedOption;
 
+                    // restart manager's selected option
+                    dialogueManager.selectedOption = "";
+
+                    if (currselectedOption == "Maybe I can see where people rehearse?")
+                    {
+                        if (classroom && library && park)
+                        {
+                            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02A" }));
+                            DP1_3 = true;
+                            DP1_2 = false;
+
+                        }
+                        else if (reherse)
+                        {
+                            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.2E" }));
+                        }
+                        else
+                        {
+                            reherse = true;
+                            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02A", "W1D2.2F" }));
+                        }
+
+                    }
+                    else if (currselectedOption == "The park!")
+                    {
+                        if (classroom && library && reherse)
+                        {
+                            DP1_3 = true;
+                            DP1_2 = false;
+                            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02B" }));
+                        }
+                        else if (park)
+                        {
+                            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.2E" }));
+                        }
+                        else
+                        {
+                            park = true;
+                            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02B", "W1D2.2F" }));
+                        }
+                    }
+
+                    else if (currselectedOption == "I think seeing a classroom would be nice before I'm forced in there against my will.")
+                    {
+                        if (library && park && reherse)
+                        {
+                            DP1_3 = true;
+                            DP1_2 = false;
+                            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02C" }));
+                        }
+                        else if (classroom)
+                        {
+                            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.2E" }));
+                        }
+                        else
+                        {
+                            classroom = true;
+                            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02C", "W1D2.2F" }));
+                        }
+                    }
+                    else if (currselectedOption == "Could we go to the library?")
+                    {
+                        if (classroom && park && reherse)
+                        {
+                            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02D" }));
+                            DP1_3 = true;
+                            DP1_2 = false;
+                        }
+                        else if (library)
+                        {
+                            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.2E" }));
+                        }
+                        else
+                        {
+                            library = true;
+                            StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.02D", "W1D2.2F" }));
+                        }
+                    }
+
+
+                    if (classroom && library && park && reherse)
+                    {
+                        DP1_3 = true;
+                    }
+
+
+                }
+
+                if (DP1_3)
+                {
+                    if (dialogueManager.activeDialogue == false)
+                    {
+                        string currselectedOption = dialogueManager.selectedOption;
+
+                        // restart manager's selected option
+                        DP1_3 = false;
+                        dialogueManager.selectedOption = "";
+
+                        StartCoroutine(dialogueManager.LoadAndStartDialoguesSequentially(new string[] { "W1D2.03" }));
+                        startTESTS = false;
+                        END = true;
+
+                    }
+
+
+                }
             }
             if (END)
             {
-                if(dialogueManager.activeDialogue == false)
+                if (dialogueManager.activeDialogue == false)
                 {
                     dialogueManager.isW1D3 = true;
+                    dialogueManager.UpdatePPref(3);
+
                 }
             }
-
-
         }
     }
+
 }
